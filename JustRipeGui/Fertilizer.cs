9870b428eb81073590162a2734e493ee0b483f96
Senaz-Dhom <zanes.tune1757@gmail.com>
2018-11-11 17:25:13 +0800

    Add new class i.e. Fertilizer

---
+++ JustRipeGui/Fertilizer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustRipeGui
+{
+    public class Fertilizer
+    {
+        int id;
+        string name;
+        int status;
+        int type;
+        int quantity;
+
+    }
+}
